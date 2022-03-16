777321dbda1cee38b65a0feadac94383a204325a
ouakassi <lmoutchoo@gmail.com>
2022-03-16 19:34:45 +0100

    adding technologies hashtags and fixing their responsevness

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,33 +4,43 @@ import SectionTitle from "../../styles/SectionTitle";
 import ProjectCard from "./ProjectCard";
 import { projects } from "./../../data/Data.js";
 
-const Projects = () => (
-  <section className="projects section" id="projects">
-    <SectionTitle
-      icon="uil uil-folder-open"
-      sectionTitle="Projects"
-      sectionSubtitle="my projects"
-    />
+const Projects = () => {
+  return (
+    <section className="projects section" id="projects">
+      <SectionTitle
+        icon="uil uil-folder-open"
+        sectionTitle="Projects"
+        sectionSubtitle="my projects"
+      />
 
-    <div className="project__container container">
-      {projects.map((project, i) => {
-        return (
-          <ProjectCard
-            key={project.id}
-            image={project.image}
-            title={project.title}
-            description={project.description}
-            github={project.github}
-            tags={project.tags}
-            website={project.website}
-          />
-        );
-      })}
-    </div>
-    <a href="google.com" className="show-more__button">
-      Show More <i className="uil uil-direction button__icon" />
-    </a>
-  </section>
-);
+      <div className="project__container container">
+        {projects.map(
+          ({ id, image, title, description, github, tags, website }, i) => {
+            return (
+              <ProjectCard
+                key={id}
+                image={image}
+                title={title}
+                description={description}
+                github={github}
+                tags={tags.map((tag, i) => {
+                  return (
+                    <span className="project__language" key={i}>
+                      #{tag}
+                    </span>
+                  );
+                })}
+                website={website}
+              />
+            );
+          }
+        )}
+      </div>
+      <a href="google.com" className="show-more__button">
+        Show More <i className="uil uil-direction button__icon" />
+      </a>
+    </section>
+  );
+};
 
 export default Projects;
--- src/data/Data.js
+++ src/data/Data.js
@@ -34,7 +34,7 @@ export const projects = [
     description:
       "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
     image: "./images/portfolio1.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
+    tags: ["React", "JaVascript", "Firebase"],
     github: "https://google.com",
     website: "https://google.com",
     id: 3,

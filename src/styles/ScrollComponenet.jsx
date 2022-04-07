4e0fbfc53da0fa80b3fb911b7df06a0857195da5
ouakassi <lmoutchoo@gmail.com>
2022-04-07 16:03:55 +0000

    adding the logic behind scroll to top button

--- src/styles/ScrollComponenet.jsx
+++ src/styles/ScrollComponenet.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ScrollComponenet.css";
 
-const ScrollComponent = () => (
-  <div href="#" className="scrollup" id="scroll-up">
-    <i className="uil uil-arrow-up scrollup__icon" />
-  </div>
-);
+const ScrollComponent = () => {
+  const [windowHeight, setwindowHeight] = useState(0);
+
+  const handlClick = () => window.scrollTo(0, 0);
+
+  useEffect(() => {
+    const onScroll = () => setwindowHeight(window.pageYOffset);
+
+    // clean up code
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  console.log(windowHeight);
+  if (windowHeight >= 400)
+    return (
+      <div onClick={handlClick} className="scrollup">
+        <i className="uil uil-arrow-up scrollup__icon" />
+      </div>
+    );
+};
 
 export default ScrollComponent;

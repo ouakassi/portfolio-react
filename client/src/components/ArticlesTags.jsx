d7eb85df9ae157313c9fc5a08353fad4aa5df74d
ouakassi <lmoutchoo@gmail.com>
2022-06-24 21:44:44 +0100

    implement more or less tags in blog page

--- client/src/components/ArticlesTags.jsx
+++ client/src/components/ArticlesTags.jsx
@@ -4,6 +4,8 @@ import { ShowMoreButton } from "./Buttons/ShowMoreButton";
 
 export default function ArticlesTags({ tagName, queryArticles }) {
   const [activeLink, setActiveLink] = useState();
+  const [tagsNumber, setTagsNumber] = useState(4);
+  const [toggleButton, setToggleButton] = useState(false);
 
   const ACTIVE_COLOR = {
     boxShadow: "var(--first-color) 0px 0px 0px 2px",
@@ -13,32 +15,53 @@ export default function ArticlesTags({ tagName, queryArticles }) {
   };
 
   return (
-    <div className="filter__container">
-      <ShowMoreButton
-        style={
-          activeLink === undefined || activeLink === ""
-            ? ACTIVE_COLOR
-            : undefined
-        }
-        method={() => {
-          queryArticles("");
-          setActiveLink("");
-        }}
-        title={"#ALL"}
-      />
-      {tagName.map((tag, i) => {
-        return (
-          <ShowMoreButton
-            style={tag === activeLink ? ACTIVE_COLOR : undefined}
-            key={i}
-            method={() => {
-              queryArticles(`tags=${tag}`);
-              setActiveLink(tag);
+    <>
+      <div className="filter__container">
+        <ShowMoreButton
+          style={
+            activeLink === undefined || activeLink === ""
+              ? ACTIVE_COLOR
+              : undefined
+          }
+          method={() => {
+            queryArticles("");
+            setActiveLink("");
+          }}
+          title={"#ALL"}
+        />
+        {tagName.map((tag, i) => {
+          return (
+            i < tagsNumber && (
+              <ShowMoreButton
+                style={tag === activeLink ? ACTIVE_COLOR : undefined}
+                key={i}
+                method={() => {
+                  queryArticles(`tags=${tag}`);
+                  setActiveLink(tag);
+                }}
+                title={`#${tag}`}
+              />
+            )
+          );
+        })}
+        {!toggleButton ? (
+          <i
+            className="icon uil uil-arrow-circle-right"
+            onClick={() => {
+              setTagsNumber(tagName.length);
+              setToggleButton(true);
             }}
-            title={`#${tag}`}
-          />
-        );
-      })}
-    </div>
+          ></i>
+        ) : (
+          <i
+            className="icon uil uil-arrow-circle-left"
+            onClick={() => {
+              setTagsNumber(4);
+              setToggleButton(false);
+            }}
+          ></i>
+        )}
+      </div>
+    </>
   );
 }
